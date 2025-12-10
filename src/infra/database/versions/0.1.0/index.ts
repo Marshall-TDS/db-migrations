624ee72017c2439afd3f5d676a2c2abb961f8607
Macbook <macbook@macbooks-air.lan>
2025-12-10 11:00:32 -0300

    fix: acrescenta todos os migrations no index

--- src/infra/database/versions/0.1.0/index.ts
+++ src/infra/database/versions/0.1.0/index.ts
@@ -4,9 +4,14 @@ import { createUserGroupsTable20251125001 } from './TASK-0002__20251125001-creat
 import { addUserPasswordColumn20251125002 } from './TASK-0003__20251125002-add-user-password-column'
 import { createRemetentesAndComunicacoesTables20250102001 } from './TASK-0004__20250102001-create-remetentes-and-comunicacoes-tables'
 import { addAssuntoHtmlToComunicacoes20250102002 } from './TASK-0005__20250102002-add-assunto-html-to-comunicacoes'
+import { refactorAccessGroups20251126001 } from './TASK-0002__20251126001-rename-user-group'
 
 export const migrations: Migration[] = [
+  createUsersTable20250101001,
   createRemetentesAndComunicacoesTables20250102001,
   addAssuntoHtmlToComunicacoes20250102002,
+  createUserGroupsTable20251125001,
+  addUserPasswordColumn20251125002,
+  refactorAccessGroups20251126001,
 ]
 
